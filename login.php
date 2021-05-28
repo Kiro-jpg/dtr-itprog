89d38e4c734a8415441bf222f73cf8030a329423
Miguel Kou <miguelito_jongco@dlsu.edu.ph>
2021-05-28 15:24:15 +0800

    tung tung tung tung

--- login.php
+++ login.php
@@ -1,381 +1,72 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Log-In</title>
+    <link rel="stylesheet" href="style\style.css">
 </head>
 
-<style>
-* {
-    font-family: -apple-system, BlinkMacSystemFont, "San Francisco", Helvetica, Arial, sans-serif;
-    font-weight: 300;
-    margin: 0;
-}
-
-html,
-body {
-    height: 100vh;
-    width: 100vw;
-    margin: 0 0;
-    display: flex;
-    align-items: flex-start;
-    justify-content: flex-start;
-    background: #f3f2f2;
-}
-
-h4 {
-    font-size: 24px;
-    font-weight: 600;
-    color: #000;
-    opacity: 0.85;
-}
-
-label {
-    font-size: 12.5px;
-    color: #000;
-    opacity: 0.8;
-    font-weight: 400;
-}
-
-form {
-    padding: 40px 30px;
-    background: #fefefe;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    padding-bottom: 20px;
-    width: 300px;
-}
-
-form h4 {
-    margin-bottom: 20px;
-    color: rgba(0, 0, 0, 0.5);
-}
-
-form h4 span {
-    color: black;
-    font-weight: 700;
-}
-
-form p {
-    line-height: 155%;
-    margin-bottom: 5px;
-    font-size: 14px;
-    color: #000;
-    opacity: 0.65;
-    font-weight: 400;
-    max-width: 200px;
-}
-
-a.discrete {
-    color: rgba(0, 0, 0, 0.4);
-    font-size: 14px;
-    border-bottom: solid 1px rgba(0, 0, 0, 0);
-    padding-bottom: 4px;
-    margin-left: auto;
-    font-weight: 300;
-    transition: all 0.3s ease;
-    margin-top: 40px;
-}
-
-a.discrete:hover {
-    border-bottom: solid 1px rgba(0, 0, 0, 0.2);
-}
-
-button {
-    -webkit-appearance: none;
-    width: auto;
-    min-width: 100px;
-    border-radius: 24px;
-    text-align: center;
-    padding: 15px 40px;
-    margin-top: 5px;
-    background-color: #b08bf8;
-    color: #fff;
-    font-size: 14px;
-    margin-left: auto;
-    font-weight: 500;
-    box-shadow: 0px 2px 6px -1px rgba(0, 0, 0, 0.13);
-    border: none;
-    transition: all 0.3s ease;
-    outline: 0;
-}
-
-button:hover {
-    transform: translateY(-3px);
-    box-shadow: 0 2px 6px -1px rgba(182, 157, 230, 0.65);
-}
-
-button:hover:active {
-    transform: scale(0.99);
-}
-
-input {
-    font-size: 16px;
-    padding: 20px 0px;
-    height: 56px;
-    border: none;
-    border-bottom: solid 1px rgba(0, 0, 0, 0.1);
-    background: #fff;
-    width: 280px;
-    box-sizing: border-box;
-    transition: all 0.3s linear;
-    color: #000;
-    font-weight: 400;
-    -webkit-appearance: none;
-}
-
-input:focus {
-    border-bottom: solid 1px #b69de6;
-    outline: 0;
-    box-shadow: 0 2px 6px -8px rgba(182, 157, 230, 0.45);
-}
-
-.floating-label {
-    position: relative;
-    margin-bottom: 10px;
-    width: 100%;
-}
-
-.floating-label label {
-    position: absolute;
-    top: calc(50% - 7px);
-    left: 0;
-    opacity: 0;
-    transition: all 0.3s ease;
-    padding-left: 44px;
-}
-
-.floating-label input {
-    width: calc(100% - 44px);
-    margin-left: auto;
-    display: flex;
-}
-
-.floating-label .icon {
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 56px;
-    width: 44px;
-    display: flex;
-}
-
-.floating-label .icon svg {
-    height: 30px;
-    width: 30px;
-    margin: auto;
-    opacity: 0.15;
-    transition: all 0.3s ease;
-}
-
-.floating-label .icon svg path {
-    transition: all 0.3s ease;
-}
-
-.floating-label input:not(:-moz-placeholder-shown) {
-    padding: 28px 0px 12px 0px;
-}
-
-.floating-label input:not(:-ms-input-placeholder) {
-    padding: 28px 0px 12px 0px;
-}
-
-.floating-label input:not(:placeholder-shown) {
-    padding: 28px 0px 12px 0px;
-}
-
-.floating-label input:not(:-moz-placeholder-shown)+label {
-    transform: translateY(-10px);
-    opacity: 0.7;
-}
-
-.floating-label input:not(:-ms-input-placeholder)+label {
-    transform: translateY(-10px);
-    opacity: 0.7;
-}
-
-.floating-label input:not(:placeholder-shown)+label {
-    transform: translateY(-10px);
-    opacity: 0.7;
-}
-
-.floating-label input:valid:not(:-moz-placeholder-shown)+label+.icon svg {
-    opacity: 1;
-}
-
-.floating-label input:valid:not(:-ms-input-placeholder)+label+.icon svg {
-    opacity: 1;
-}
-
-.floating-label input:valid:not(:placeholder-shown)+label+.icon svg {
-    opacity: 1;
-}
-
-.floating-label input:valid:not(:-moz-placeholder-shown)+label+.icon svg path {
-    fill: #b69de6;
-}
-
-.floating-label input:valid:not(:-ms-input-placeholder)+label+.icon svg path {
-    fill: #b69de6;
-}
-
-.floating-label input:valid:not(:placeholder-shown)+label+.icon svg path {
-    fill: #b69de6;
-}
-
-.floating-label input:not(:valid):not(:focus)+label+.icon {
-    -webkit-animation-name: shake-shake;
-    animation-name: shake-shake;
-    -webkit-animation-duration: 0.3s;
-    animation-duration: 0.3s;
-}
-
-@-webkit-keyframes shake-shake {
-    0% {
-        transform: translateX(-3px);
-    }
-
-    20% {
-        transform: translateX(3px);
-    }
-
-    40% {
-        transform: translateX(-3px);
-    }
-
-    60% {
-        transform: translateX(3px);
-    }
-
-    80% {
-        transform: translateX(-3px);
-    }
-
-    100% {
-        transform: translateX(0px);
-    }
-}
-
-@keyframes shake-shake {
-    0% {
-        transform: translateX(-3px);
-    }
-
-    20% {
-        transform: translateX(3px);
-    }
-
-    40% {
-        transform: translateX(-3px);
-    }
-
-    60% {
-        transform: translateX(3px);
-    }
-
-    80% {
-        transform: translateX(-3px);
-    }
-
-    100% {
-        transform: translateX(0px);
-    }
-}
-
-.session {
-    display: flex;
-    flex-direction: row;
-    width: auto;
-    height: auto;
-    margin: auto auto;
-    background: #ffffff;
-    border-radius: 4px;
-    box-shadow: 0px 2px 6px -1px rgba(0, 0, 0, 0.12);
-}
-
-.left {
-    width: 220px;
-    height: auto;
-    min-height: 100%;
-    position: relative;
-    background-image: url("https://images.pexels.com/photos/114979/pexels-photo-114979.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940");
-    background-size: cover;
-    border-top-left-radius: 4px;
-    border-bottom-left-radius: 4px;
-}
-
-.left svg {
-    height: 40px;
-    width: auto;
-    margin: 20px;
-}
-
-.flash {
-    margin-bottom: 5px;
-    margin-top: 1rem;
-    font-size: 12px;
-    color: red;
-    opacity: 0.65;
-    font-weight: 400;
-    margin-left: 3.5rem;
-}
-</style>
-
 <body>
-    <div class="session">
-        <div class="left">
-        </div>
-        <form name="frm" method="post" action="check.php">
-            <h4>We are <span>TRIO</span></h4>
-            <p>Welcome back! Log in to your account to view today's schedule.</p>
-            <div class="floating-label">
-                <input placeholder="Username" type="text" name="username" autocomplete="off">
-                <label for="username">Username:</label>
-                <div class="icon">
-                    <svg enable-background="new 0 0 100 100" version="1.1" viewBox="0 0 100 100" xml:space="preserve"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <style type="text/css">
-                        .st0 {
-                            fill: none;
-                        }
-                        </style>
-                        <g transform="translate(0 -952.36)">
-                            <path
-                                d="m17.5 977c-1.3 0-2.4 1.1-2.4 2.4v45.9c0 1.3 1.1 2.4 2.4 2.4h64.9c1.3 0 2.4-1.1 2.4-2.4v-45.9c0-1.3-1.1-2.4-2.4-2.4h-64.9zm2.4 4.8h60.2v1.2l-30.1 22-30.1-22v-1.2zm0 7l28.7 21c0.8 0.6 2 0.6 2.8 0l28.7-21v34.1h-60.2v-34.1z" />
-                        </g>
-                        <rect class="st0" width="100" height="100" />
-                    </svg>
-
+    <div class="big">
+        <div class="logincont">
+            <div class="session">
+                <div class="left">
                 </div>
-            </div>
-            <div class="floating-label">
-                <input placeholder="Password" type="password" name="pass" autocomplete="off">
-                <label for="pass">Password:</label>
-                <div class="icon">
+                <form name="frm" method="post" action="check.php">
+                    <h4>We are <span>TRIO</span></h4>
+                    <p>Welcome back! Log in to your account to view today's schedule.</p>
+                    <div class="floating-label">
+                        <input placeholder="Username" type="text" name="username" autocomplete="off">
+                        <label for="username">Username:</label>
+                        <div class="icon">
+                            <svg enable-background="new 0 0 100 100" version="1.1" viewBox="0 0 100 100"
+                                xml:space="preserve" xmlns="http://www.w3.org/2000/svg">
+                                <style type="text/css">
+                                .st0 {
+                                    fill: none;
+                                }
+                                </style>
+                                <g transform="translate(0 -952.36)">
+                                    <path
+                                        d="m17.5 977c-1.3 0-2.4 1.1-2.4 2.4v45.9c0 1.3 1.1 2.4 2.4 2.4h64.9c1.3 0 2.4-1.1 2.4-2.4v-45.9c0-1.3-1.1-2.4-2.4-2.4h-64.9zm2.4 4.8h60.2v1.2l-30.1 22-30.1-22v-1.2zm0 7l28.7 21c0.8 0.6 2 0.6 2.8 0l28.7-21v34.1h-60.2v-34.1z" />
+                                </g>
+                                <rect class="st0" width="100" height="100" />
+                            </svg>
 
-                    <svg enable-background="new 0 0 24 24" version="1.1" viewBox="0 0 24 24" xml:space="preserve"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <style type="text/css">
-                        .st0 {
-                            fill: none;
-                        }
+                        </div>
+                    </div>
+                    <div class="floating-label">
+                        <input placeholder="Password" type="password" name="pass" autocomplete="off">
+                        <label for="pass">Password:</label>
+                        <div class="icon">
 
-                        .st1 {
-                            fill: #010101;
-                        }
-                        </style>
-                        <rect class="st0" width="24" height="24" />
-                        <path class="st1" d="M19,21H5V9h14V21z M6,20h12V10H6V20z" />
-                        <path class="st1"
-                            d="M16.5,10h-1V7c0-1.9-1.6-3.5-3.5-3.5S8.5,5.1,8.5,7v3h-1V7c0-2.5,2-4.5,4.5-4.5s4.5,2,4.5,4.5V10z" />
-                        <path class="st1"
-                            d="m12 16.5c-0.8 0-1.5-0.7-1.5-1.5s0.7-1.5 1.5-1.5 1.5 0.7 1.5 1.5-0.7 1.5-1.5 1.5zm0-2c-0.3 0-0.5 0.2-0.5 0.5s0.2 0.5 0.5 0.5 0.5-0.2 0.5-0.5-0.2-0.5-0.5-0.5z" />
-                    </svg>
-                </div>
+                            <svg enable-background="new 0 0 24 24" version="1.1" viewBox="0 0 24 24"
+                                xml:space="preserve" xmlns="http://www.w3.org/2000/svg">
+                                <style type="text/css">
+                                .st0 {
+                                    fill: none;
+                                }
 
-            </div>
-            <button type="submit" value="Login" name="loginBtn">Log in</button>
-            <?php
+                                .st1 {
+                                    fill: #010101;
+                                }
+                                </style>
+                                <rect class="st0" width="24" height="24" />
+                                <path class="st1" d="M19,21H5V9h14V21z M6,20h12V10H6V20z" />
+                                <path class="st1"
+                                    d="M16.5,10h-1V7c0-1.9-1.6-3.5-3.5-3.5S8.5,5.1,8.5,7v3h-1V7c0-2.5,2-4.5,4.5-4.5s4.5,2,4.5,4.5V10z" />
+                                <path class="st1"
+                                    d="m12 16.5c-0.8 0-1.5-0.7-1.5-1.5s0.7-1.5 1.5-1.5 1.5 0.7 1.5 1.5-0.7 1.5-1.5 1.5zm0-2c-0.3 0-0.5 0.2-0.5 0.5s0.2 0.5 0.5 0.5 0.5-0.2 0.5-0.5-0.2-0.5-0.5-0.5z" />
+                            </svg>
+                        </div>
+
+                    </div>
+
+                    <button type="submit" value="Login" name="loginBtn">Log in</button>
+                    <?php
 if (isset($_GET["error"])) {
     $error = $_GET["error"];
 
@@ -386,9 +77,44 @@ if (isset($_GET["error"])) {
 }
 
 ?>
-        </form>
-    </div>
+                </form>
+            </div>
+        </div>
 
+        <div class="chatcont">
+            <div class="feedback-card">
+                <div class="feedback-header">
+                    Simple Chat Client
+                </div>
+
+                <form method="POST" class="feedback-body">
+                    <textarea ,type="text" class="feedback-body__email" placeholder="Client Message"
+                        name="txtMessage"></textarea>
+                    <?php
+$host = "127.0.0.1";
+$port = 50001;
+set_time_limit(0);
+if (isset($_POST["btnSend"])) {
+
+    $msg = $_REQUEST["txtMessage"];
+    $sock = socket_create(AF_INET, SOCK_STREAM, 0);
+    socket_connect($sock, $host, $port);
+
+    socket_write($sock, $msg, strlen($msg));
+
+    $reply = socket_read($sock, 1924);
+    $reply = trim($reply);
+    $reply = "Server says:\n" . $reply;
+}
+?>
+                    <textarea ,type="text" class="feedback-body__message"
+                        placeholder="Server Reply:"><?php echo @$reply; ?></textarea>
+                    <button type="submit" name="btnSend" class="feedback-body__submit">SEND</button>
+                </form>
+
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
